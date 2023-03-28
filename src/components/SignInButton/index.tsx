4b4a32930716be665c4395c6231d9098e3261932
EricChandia <eric.chandia.ti@gmail.com>
2023-03-27 22:46:28 -0300

    feat: implemented github session login

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,24 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export function SignInButton(){
-    const isUserLoggedIn = true;
+    const { data: session, status } = useSession();
 
-    return isUserLoggedIn ? (
+    console.log(session);
+
+    return status === 'authenticated' ? (
         <button type="button" className={styles.signInButton}>
             <FaGithub color='#04d361'/>
-            Eric Chandia
-            <FiX color='#737380' className={styles.closeIcon}/>
+            {session.user?.name}
+            <FiX color='#737380' className={styles.closeIcon} onClick={() => signOut()}/>
         </button>
     ) : (
-        <button type="button" className={styles.signInButton}>
+        <button 
+        type="button" 
+        className={styles.signInButton}
+        onClick={() => signIn('github')}>
             <FaGithub color='#eba417'/>
             Sign in with Github
         </button>
