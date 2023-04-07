a81c609d51987ec570d9b3514d3132dde51ad8eb
EricChandia <eric.chandia.ti@gmail.com>
2023-04-07 14:23:06 -0300

    feat: github login implemented

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
+import { NextAuthOptions } from "next-auth";
 
-
-export default NextAuth({
+export const authOptions: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID || '',
@@ -12,6 +14,45 @@ export default NextAuth({
         params: { scope: "read:user" },
       },
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }): Promise<boolean>{
+      const email:string = user.email ?? '';
+
+      console.log(email);
+
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+
+        return true;
+
+        }catch{
+          return false;
+        }
+    },
+  }
   
-});
+};
+
+export default NextAuth(authOptions)
