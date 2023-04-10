797c172d0ce1d889304c318dd2bc15b8f463a8be
EricChandia <eric.chandia.ti@gmail.com>
2023-04-09 21:13:59 -0300

    feat: implemented activeLink component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,25 @@
 import Image from "next/image";
 import { SignInButton } from "../SignInButton";
 import styles from './styles.module.scss';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { ActiveLink } from "../ActiveLink";
 
 export function Header(){
+    const { asPath } = useRouter();
+
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <Image src="/images/vercel.svg" alt="ig.news" width={50} height={50}/>
             <nav>
-                <a>Home</a>
-                <a>Posts</a>
+                <ActiveLink href='/' activeClassName={styles.active}>
+                    <div>Home</div>
+                </ActiveLink>
+                {/* <Link href='/posts' prefetch> <- desta forma irá carregar o posts antes de recusar abri-lo*/}
+                <ActiveLink href='/posts' activeClassName={styles.active}>
+                    <div>Posts</div>
+                </ActiveLink>
             </nav>
 
             <SignInButton />
