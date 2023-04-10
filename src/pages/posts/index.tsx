0298e47067243842f4a0a6a8f3b36096f13cdec9
EricChandia <eric.chandia.ti@gmail.com>
2023-04-09 21:15:02 -0300

    feat:  implemented posts page

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,72 @@
+import { GetStaticProps } from "next";
+import { prismic_client } from "../../services/prismic";
+import Head from "next/head";
+import styles from './styles.module.scss';
+import { RichText } from "prismic-dom";
+import Link from "next/link";
+
+type Post = {
+    slug: string,
+    title: string,
+    excerpt: string,
+    updatedAt: string
+}
+
+interface PostProps{
+    posts: Post[]
+}
+
+export default function Posts({ posts } : PostProps){
+    return(
+        <>
+            <Head>
+                <title>Posts | React-Posts</title>
+            </Head>
+
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    { posts.map((post:Post) => (
+                        <Link href={`/posts/${post.slug}`} key={post.slug}>
+                            <time>
+                            {post.updatedAt}
+                            </time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </Link>
+                    )) }
+                </div>
+            </main>
+        </>
+    )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = prismic_client;
+
+    const results = await prismic_client.getAllByType('publication', 
+    { 
+        fetchLinks : ['publication.title', 'publication.content'],
+        pageSize: 100 
+    } );
+
+    const posts = results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-br', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    });
+
+    // console.log(results);
+
+    return {
+        props: {
+            posts
+        }
+    }
+}
