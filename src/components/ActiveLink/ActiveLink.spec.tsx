483bbaa481586d33c85d62b7e6a1f12c7d0d152b
EricChandia <eric.chandia.ti@gmail.com>
2023-04-12 18:56:11 -0300

    test: ActiveLink test implemented

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,41 @@
+import { render } from '@testing-library/react';
+import { ActiveLink } from '.';
+import {screen} from '@testing-library/dom'
+
+
+jest.mock('next/router', () => {
+    return{
+        useRouter(){
+            return {
+                asPath: '/'
+            }
+        }
+    }
+});
+
+describe('Active link component', () => {
+    it('renders correctly', () => {
+        const { getByText } = render(
+            <ActiveLink href='/' activeClassName='active'>
+                <span>Home</span>
+            </ActiveLink>
+        )
+    
+        expect(getByText('Home')).toBeInTheDocument();
+    });
+    
+    
+    test('is receiving active class', () => {
+        const { getByTestId } = render(
+            <ActiveLink data-testid='active-link' href='/' activeClassName='active'>
+                <span>Home</span>
+            </ActiveLink>
+        )
+    
+        const element = getByTestId('active-link');
+    
+        expect(element).toHaveClass('active');
+    });
+    
+});
+
