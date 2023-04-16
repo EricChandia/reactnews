32605da0faac996e1e851e5b1de386e37ef71598
EricChandia <eric.chandia.ti@gmail.com>
2023-04-16 00:13:49 -0300

    chore: tests config and packages updated

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,8 @@ module.exports = {
         '^.+\\.(js|jsx|ts|tsx)$': "<rootDir>/node_modules/babel-jest"
     },
     moduleNameMapper: {
-        '\\.(scss|css|sass)$': "identity-obj-proxy"
+        '\\.(scss|css|sass)$': "identity-obj-proxy",
+        "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tests/__mocks__/fileMock.js",
     },
     testEnvironment: 'jsdom'
 };
